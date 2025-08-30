268e5e789571d77c5f1a71cd4720e1b32ee792c4
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-08-30 17:29:14 +0700

    Init service

---
+++ plane_info_service/src/main/java/dev/skyherobrine/services/PlaneInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneInfoServiceApplication.class, args);
+    }
+
+}
---
+++ plane_info_service/src/test/java/dev/skyherobrine/services/PlaneInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
