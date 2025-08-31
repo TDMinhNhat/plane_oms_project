e9367d70d0156d0d055280a55721fb7911484562
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-08-31 09:00:36 +0700

    Add configs

---
+++ library_core/src/main/java/dev/skyherobrine/library/configs/JpaConfig.java
@@ -0,0 +1,11 @@
+package dev.skyherobrine.library.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "dev.skyherobrine.services.repositories")
+public class JpaConfig {
+}
