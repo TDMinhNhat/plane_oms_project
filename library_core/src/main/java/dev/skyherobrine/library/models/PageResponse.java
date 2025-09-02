13bacd330eeb5b9837cef64f757ab489789948b4
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-02 16:19:09 +0700

    Add library and page

---
+++ library_core/src/main/java/dev/skyherobrine/library/models/PageResponse.java
@@ -0,0 +1,17 @@
+package dev.skyherobrine.library.models;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class PageResponse extends PageImpl<Object> {
+
+    public PageResponse(List<Object> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public PageResponse(List<Object> content) {
+        super(content);
+    }
+}
