7c882ec875bd33341bb15c7ad62f9f581d56d373
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-14 19:00:36 +0700

    Init server

---
+++ server_eureka/src/test/java/dev/tdminhnhat/server/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tdminhnhat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
